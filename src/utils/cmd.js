1d03aa48217f7c43366766dff63cdc66cbef9eab
chasethemoon0509 <2762851151@qq.com>
2021-11-18 23:13:40 +0800

    2021-11-*18

--- src/utils/cmd.js
+++ src/utils/cmd.js
@@ -1,12 +1,52 @@
-const  { exec } = require("child_process")
+const { execSync } = require("child_process")
 
-const scrcpyPath = "cd ./public/scrcpy && "
+class eS {
+    constructor () {
+        this.resultMsg = ""
+    }
 
-const getDevices =  exec(scrcpyPath + "adb devices", (error, stdout, stderr) => {
-        console.log(`执行错误: ${error}`);
-        console.log("stdout的结果：", stdout);
-        console.log("stderr的结果：", stderr);
-        return stdout
-})
+    // 最基本的命令执行方法
+    base (cmdStr) {
+        return execSync(`cd ../../public/scrcpy && ${cmdStr}`).toString()
+    }
 
-export default getDevices
+    // 获取设备号和状态方法
+    getSerialStatus () {
+        // deviceAll 存放所有的设信息
+        let deviceAll = []
+        // item 存放 1 个设备信息，并以一个独立的设备信息对象，插入到 deviceAll 数组中
+        let item = {}
+        let cmdResult = this.base("adb devices").split("\n")
+        cmdResult.shift()
+        cmdResult.pop()
+        cmdResult.pop()
+        for (let i = 0; i < cmdResult.length; i++) {
+            item["serial"] = cmdResult[i].replace("\r", "").split("\t")[0]
+            item["status"] = cmdResult[i].replace("\r", "").split("\t")[1]
+            deviceAll[i] = item
+        }
+        return deviceAll
+    }
+
+//     获取系统版本方法
+    getSystemVersion () {
+        let baseInfo = this.getSerialStatus()
+        for (let i = 0; i < baseInfo.length; i++) {
+            baseInfo[i]["version"] = this.base(`adb -s ${baseInfo[i]["serial"]} shell getprop ro.build.version.release`).replace("\r\n", "")
+            baseInfo[i]["brand"] = this.base(`adb -s ${baseInfo[i]["serial"]} shell getprop ro.product.brand`).replace("\r\n", "")
+            baseInfo[i]["name"] = this.base(`adb -s ${baseInfo[i]["serial"]} shell getprop ro.product.name`).replace("\r\n", "")
+        }
+        // for (let i = 0; i < baseInfo.length; i++) {
+        //     baseInfo[i]["brand"] = this.base(`adb -s ${baseInfo[i]["serial"]} shell getprop ro.product.brand`).replace("\r\n", "")
+        // }
+        // for (let i = 0; i < baseInfo.length; i++) {
+        //     baseInfo[i]["name"] = this.base(`adb -s ${baseInfo[i]["serial"]} shell getprop ro.product.name`).replace("\r\n", "")
+        // }
+        return baseInfo   
+        
+    }
+
+}
+
+let a = new eS()
+console.log(a.getSerialStatus());
